ba5533c342f37e33f8ec2a98002ebedcbd0ef5ec
FredSchuck <fredschuck@gmail.com>
2024-04-21 19:01:13 -0400

    Added api route

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
 mongoose
   .connect(process.env.MONGO_URI as string)
@@ -11,10 +12,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello World!" });
-});
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7001, () => {
-  console.log("Server is running on https://localhost:7001");
+  console.log("Server is running on http://localhost:7001");
 });
