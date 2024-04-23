2d6a0212f6744381b26cf775d53fcd196e775e65
FredSchuck <fredschuck@gmail.com>
2024-04-22 23:33:51 -0400

    Added jwtCheck

--- server/src/routes/MyUserRoute.ts
+++ server/src/routes/MyUserRoute.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import { jwtCheck } from "../middleware/auth";
 import MyUserController from "../controllers/MyUserController";
 
 const router = express.Router();
 
-router.post("/", MyUserController.createCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
 
 export default router;
