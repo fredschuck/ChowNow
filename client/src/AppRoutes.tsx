ff83ab402cfa7f5d4730f50c17c69b6a8a69d3f2
FredSchuck <fredschuck@gmail.com>
2024-05-09 22:25:57 -0400

    Added UserProfilePage route

--- client/src/AppRoutes.tsx
+++ client/src/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Layout from "./layouts/layout";
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
 
 const AppRoutes = () => {
   return (
@@ -9,13 +10,20 @@ const AppRoutes = () => {
       <Route
         path="/"
         element={
-          <Layout>
+          <Layout showHero>
             <HomePage />
           </Layout>
         }
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
+      <Route
+        path="/user-profile"
+        element={
+          <Layout>
+            <UserProfilePage />
+          </Layout>
+        }
+      />
       {/* Redirect to home page whenever any other path is visited*/}
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
